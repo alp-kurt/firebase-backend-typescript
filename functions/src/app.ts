cd9b762c8ad6a8563a84572b9cca45d427e4d8c0
Alp Kurt <krtalp@gmail.com>
2026-02-04 15:27:07 +0100

    chore: enable cors

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { sessionsRouter } from "./routes/sessions.routes";
 import { HttpError, sendError } from "./utils/http";
 
 export const app = express();
 
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use("/api", sessionsRouter);
 
